67c76f749fd05debac36ec3a39264a94bae1af74
Rogier Nitschelm <rogiernitschelm@live.nl>
2016-05-21 11:14:50 +0200

    add form partials and extra model validations

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,6 @@
+// intial state of the form
+// employer info hidden
+// 
+// $(document).ready(function() {
+//   $('.employer_form').hide();
+// });
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_presence_of :first_name, :last_name
+  validates_presence_of  :type, on: :create
 
   def self.types
     %w(Employer Freelancer)
