501c0040e8c1f55678c24eef08e5431f5deae9dd
Rogier Nitschelm <rogiernitschelm@live.nl>
2016-05-21 21:55:40 +0200

    add extra columns to job model. Move some common form fields to a shared partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519102821) do
+ActiveRecord::Schema.define(version: 20160521195233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 20160519102821) do
     t.string   "title"
     t.date     "start_date"
     t.integer  "employer_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "offered_pay_rate"
+    t.text     "job_description"
   end
 
   add_index "jobs", ["employer_id"], name: "index_jobs_on_employer_id", using: :btree
